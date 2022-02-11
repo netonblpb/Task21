50d6c78cb2f79414ed1d620685b4b78d11cfcdf9
netonblpb <skullbog@gmail.com>
2022-02-11 11:15:49 +0300

    Task21 complete

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine();
+
+if (number.Length != 5) Console.WriteLine("Неправильное число!");
+
+else if (number[0] == number [4] && number[1] == number[3]) Console.WriteLine("введенное число - палиндром");
+
+else Console.WriteLine("Ввведенное число - не палиндром");
